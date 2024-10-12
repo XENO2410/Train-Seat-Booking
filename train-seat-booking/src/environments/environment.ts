f1c9418db3bf1d72d75f4c1608daae42b2dc4998
Tarun <97669140+XENO2410@users.noreply.github.com>
2024-10-12 13:00:15 +0530

    Render URL update

--- train-seat-booking/src/environments/environment.ts
+++ train-seat-booking/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api' // Update this to your backend API URL
+    apiUrl: 'https://train-seat-booking-mmqj.onrender.com/api' // Update this to your backend API URL
   };
